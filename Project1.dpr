342239fa15ffdd83a909579e551f8e52dfade129
SHIN Jaeguk <shinjguk@naver.com>
2020-09-27 23:51:11 +0900

    Add files via upload

--- Project1.dpr
+++ Project1.dpr
@@ -3,11 +3,11 @@ program Project1;
 uses
   Vcl.Forms,
   Unit1 in 'Unit1.pas' {Form1},
-  ID3v2Library in '..\..\..\programming\delphi\id3\ID3v2Library.pas',
-  ID3v1Library in '..\..\..\programming\delphi\id3\ID3v1Library.pas',
-  BufferedStream in '..\..\..\programming\delphi\id3\BufferedStream.pas',
-  bass in '..\..\..\programming\delphi\bass\delphi\bass.pas',
-  uTExtendedX87 in '..\..\..\programming\delphi\id3\uTExtendedX87.pas';
+  bass in 'bass.pas',
+  BufferedStream in 'BufferedStream.pas',
+  ID3v1Library in 'ID3v1Library.pas',
+  ID3v2Library in 'ID3v2Library.pas',
+  uTExtendedX87 in 'uTExtendedX87.pas';
 
 {$R *.res}
 
